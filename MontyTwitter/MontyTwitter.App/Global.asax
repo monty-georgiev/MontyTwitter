48c300309f318fda603a0a2c25ccf196aebc810d
Martin Georgiev <monty.georgiev@gmail.com>
2015-10-15 14:26:23 +0300

    Data Layer
    Created structure for data layer of the application

---
+++ MontyTwitter/MontyTwitter.App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MontyTwitter.App.MvcApplication" Language="C#" %>
---
+++ MontyTwitter/MontyTwitter.App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MontyTwitter.App.Startup))]
+namespace MontyTwitter.App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
