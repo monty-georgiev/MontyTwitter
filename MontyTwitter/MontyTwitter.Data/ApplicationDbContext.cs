8ee30233cac9d7ad4fed8b82dab28236134e3b93
Martin Georgiev <monty.georgiev@gmail.com>
2015-10-15 15:16:59 +0300

    Update
    Removed some of the initial actions in HomeController; created
    BaseController; Changed config file to rename DB instance in MSSQL
    Server;

---
+++ MontyTwitter/MontyTwitter.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MontyTwitter.Models;
+
+namespace MontyTwitter.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
